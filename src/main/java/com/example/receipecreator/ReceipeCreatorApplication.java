c068ee06a2cbc02466680a5417c70ce80bde1a88
Jaypad07 <jaypad07@gmail.com>
2023-06-13 15:54:00 -0700

    first commit

---
+++ src/main/java/com/example/receipecreator/ReceipeCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.receipecreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipeCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceipeCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/receipecreator/ReceipeCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.receipecreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipeCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
